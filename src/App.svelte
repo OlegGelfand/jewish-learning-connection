b999ae2f5ab30543e8b52ce233dbb1cd9f99a677
Oleg Gelfand <oleggelfand.1@gmail.com>
2022-06-13 00:31:00 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,144 @@
+
+<header>
+  <h1 class="header-title"> JLC Lectures </h1>
+</header>
+
+<div class="speakers-nav">
+<!--  <div class="speakers">-->
+  <div class="individual-speaker"><a class="speaker-links" href="#">Rabbi Ephraim Nisenbaum</a></div>
+  <div class="individual-speaker"><a class="speaker-links" href="#">Rabbi Moshe Stoll</a></div>
+  <div class="individual-speaker"><a class="speaker-links" href="#">Rabbi Yossi Nisenbaum</a></div>
+<!--  </div>-->
+</div>
+<section>
+  <div class="series-container">
+    <h2 class="individual-class-container-title"> The Guide to the perplexed</h2>
+    <div class="individual-class-info">
+      <p class="title">The Guide to the Perplexed</p>
+      <p class="chapter">Chapter 2</p>
+      <p class="speaker">Rabbi Ephraim Nisenbaum</p>
+      <p class="upload-date">June 4, 2022</p>
+      <p class="file-size">16.3MB</p>
+    </div>
+    <br>
+  </div>
+
+  <div class="individual-class-info">
+    <p class="title">The Guide to the Perplexed</p>
+    <p class="chapter">Chapter 2</p>
+    <p class="speaker">Rabbi Ephraim Nisenbaum</p>
+    <p class="upload-date">June 4, 2022</p>
+    <p class="file-size">16.3MB</p>
+
+  </div>
+  <br>
+  <div class="individual-class-info">
+    <p class="title">The Guide to the Perplexed</p>
+    <p class="chapter">Chapter 2</p>
+    <p class="speaker">Rabbi Ephraim Nisenbaum</p>
+    <p class="upload-date">June 4, 2022</p>
+    <p class="file-size">16.3MB</p>
+
+  </div>
+  <div class="series-container">
+    <h2 class="individual-class-container-title"> Bereishis and Man</h2>
+    <div class="individual-class-info">
+      <p class="title">Bereishis and Man</p>
+      <p class="chapter">Chapter 2</p>
+      <p class="speaker">Rabbi Ephraim Nisenbaum</p>
+      <p class="upload-date">June 4, 2022</p>
+      <p class="file-size">16.3MB</p>
+
+    </div>
+    <br>
+  </div>
+  <div class="individual-class-info">
+    <p class="title">Bereishis and Man</p>
+    <p class="chapter">Chapter 2</p>
+    <p class="speaker">Rabbi Ephraim Nisenbaum</p>
+    <p class="upload-date">June 4, 2022</p>
+    <p class="file-size">16.3MB</p>
+
+  </div>
+  <br>
+  <div class="individual-class-info">
+    <p class="title">Bereishis and Man</p>
+    <p class="chapter">Chapter 2</p>
+    <p class="speaker">Rabbi Ephraim Nisenbaum</p>
+    <p class="upload-date">June 4, 2022</p>
+    <p class="file-size">16.3MB</p>
+    </div>
+
+</section>
+<style>
+  header {
+    height:50px;
+  }
+  .individual-class-container-title {
+    text-align: center;
+    font-size: 14px;
+    font-weight: 400;
+    font-family: "Inter";
+  }
+  .header-title {
+    text-align: center;
+    width:100%;
+    font-weight: 400;
+    font-family: "Inter";
+    padding: 0% 0px 1% 0px;
+    margin-top:2px;
+
+  }
+  .speakers-nav {
+    display: flex;
+    background-color:#9A1E1E;
+    padding:1% 15% 1% 15%;
+    text-align: center;
+
+    color:white;
+  }
+
+  .individual-speaker {
+    width:100%;
+
+  ;
+
+  }
+ .speaker-links {
+    align-content: center;
+   color: white;
+   text-decoration: none;
+   align-self: center;
+ }
+.series-container {
+  flex-direction: row;
+}
+
+  .title, .chapter, .speaker, .upload-date, .file-size {
+   align-content: center;
+    font-size: 12px;
+    margin:0;
+  }
+
+  .individual-class-info {
+    flex-direction: row;
+    text-align: center;
+    border:2px solid black;
+    border-radius: 5%;
+    background-color: white;
+    width:150px;
+    padding:5px 10px 5px 10px;
+
+  }
+
+  section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    /*width:15%;*/
+    margin-right:40%;
+    margin-left:40%;
+
+  }
+
+</style>
