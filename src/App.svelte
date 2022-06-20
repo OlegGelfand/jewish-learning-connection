6845dc59465385717467042229d13389b9647e7b
Oleg Gelfand <oleggelfand.1@gmail.com>
2022-06-20 01:57:41 -0400

    style changes made

--- src/App.svelte
+++ src/App.svelte
@@ -14,79 +14,104 @@
   <div class="series-container">
     <h2 class="individual-class-container-title"> The Guide to the perplexed</h2>
     <div class="individual-class-info">
+      <a class="link-to-site-button" href="https://www.youtube.com/watch?v=bYfGoO92sQ4">
       <p class="title">The Guide to the Perplexed</p>
       <p class="chapter">Chapter 2</p>
       <p class="speaker">Rabbi Ephraim Nisenbaum</p>
       <p class="upload-date">June 4, 2022</p>
       <p class="file-size">16.3MB</p>
+      </a>
     </div>
     <br>
   </div>
 
   <div class="individual-class-info">
+    <a class="link-to-site-button" href="https://www.youtube.com/watch?v=bYfGoO92sQ4">
     <p class="title">The Guide to the Perplexed</p>
     <p class="chapter">Chapter 2</p>
     <p class="speaker">Rabbi Ephraim Nisenbaum</p>
     <p class="upload-date">June 4, 2022</p>
     <p class="file-size">16.3MB</p>
+    </a>
 
   </div>
   <br>
   <div class="individual-class-info">
+    <a class="link-to-site-button" href="https://www.youtube.com/watch?v=bYfGoO92sQ4">
     <p class="title">The Guide to the Perplexed</p>
     <p class="chapter">Chapter 2</p>
     <p class="speaker">Rabbi Ephraim Nisenbaum</p>
     <p class="upload-date">June 4, 2022</p>
     <p class="file-size">16.3MB</p>
+    </a>
 
   </div>
   <div class="series-container">
     <h2 class="individual-class-container-title"> Bereishis and Man</h2>
     <div class="individual-class-info">
+      <a class="link-to-site-button" href="https://www.youtube.com/watch?v=bYfGoO92sQ4">
       <p class="title">Bereishis and Man</p>
       <p class="chapter">Chapter 2</p>
       <p class="speaker">Rabbi Ephraim Nisenbaum</p>
       <p class="upload-date">June 4, 2022</p>
       <p class="file-size">16.3MB</p>
+      </a>
 
     </div>
     <br>
   </div>
+
   <div class="individual-class-info">
+    <a class="link-to-site-button" href="https://www.youtube.com/watch?v=bYfGoO92sQ4">
     <p class="title">Bereishis and Man</p>
     <p class="chapter">Chapter 2</p>
     <p class="speaker">Rabbi Ephraim Nisenbaum</p>
     <p class="upload-date">June 4, 2022</p>
     <p class="file-size">16.3MB</p>
+      </a>
+
+
 
   </div>
+
   <br>
   <div class="individual-class-info">
+    <a class="link-to-site-button" href="https://www.youtube.com/watch?v=bYfGoO92sQ4">
     <p class="title">Bereishis and Man</p>
     <p class="chapter">Chapter 2</p>
     <p class="speaker">Rabbi Ephraim Nisenbaum</p>
-    <p class="upload-date">June 4, 2022</p>
+    <p class="upload-date" ></p>
     <p class="file-size">16.3MB</p>
+    </a>
     </div>
 
 </section>
+
 <style>
+
   header {
-    height:50px;
+    height:60px;
+    background-color: #D9D9D9;
   }
   .individual-class-container-title {
     text-align: center;
-    font-size: 14px;
+    font-size: 20px;
     font-weight: 400;
-    font-family: "Inter";
+    font-family: "Inter", sans-serif;
   }
+  .link-to-site-button{
+    text-decoration: none;
+    color:black;
+
+  }
+
   .header-title {
     text-align: center;
     width:100%;
     font-weight: 400;
-    font-family: "Inter";
-    padding: 0% 0px 1% 0px;
-    margin-top:2px;
+    font-family: 'Inter', sans-serif;
+    margin-bottom:5%;
+    margin-top:2%
 
   }
   .speakers-nav {
@@ -100,45 +125,74 @@
 
   .individual-speaker {
     width:100%;
-
-  ;
-
   }
- .speaker-links {
+  .speaker-links {
     align-content: center;
    color: white;
    text-decoration: none;
    align-self: center;
  }
-.series-container {
+  .speaker-links:hover {
+  align-content: center;
+  color: black;
+  text-decoration: none;
+  align-self: center;
+  }
+  .series-container {
   flex-direction: row;
-}
 
+  }
+
+  .title {
+  padding-top:5%;
+  }
+  .file-size {
+  padding-bottom:5%;
+}
   .title, .chapter, .speaker, .upload-date, .file-size {
    align-content: center;
-    font-size: 12px;
+    font-size: 18px;
     margin:0;
+    /*color:black;*/
+    padding-left:10%;
+    line-height: 30px;
+  }
+  .title, .chapter, .speaker, .upload-date, .file-size:hover {
+    align-content: center;
+    font-size: 18px;
+    margin:0;
+    padding-left:10%;
+    line-height: 30px;
+    /*color:white;*/
   }
 
   .individual-class-info {
     flex-direction: row;
-    text-align: center;
-    border:2px solid black;
     border-radius: 5%;
     background-color: white;
-    width:150px;
+    width:250px;
+    height:80%;
     padding:5px 10px 5px 10px;
-
+    box-shadow: 1px 5px 5px 5px gray;
+  }
+  .individual-class-info:hover {
+    flex-direction: row;
+    border-radius: 5%;
+    background-color: #9A1E1E;
+    width:250px;
+    padding:5px 10px 5px 10px;
+    box-shadow: 1px 5px 5px 5px gray;
   }
 
   section {
     display: flex;
     flex-direction: column;
     align-items: center;
-    /*width:15%;*/
     margin-right:40%;
     margin-left:40%;
-
   }
-
+ç
 </style>
+<script>
+
+</script>
